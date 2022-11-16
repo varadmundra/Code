ad8a5b93b2b8a46ac1a9f5a31c1c680e2cc31446
Varad Mundra <varadmundra@gmail.com>
2022-11-16 14:34:51 +0530

    edit

--- tokens.js
+++ tokens.js
@@ -1,13 +1,28 @@
 
+windows.onload = function () {
+    if (windows.ethereum !== "undefined") {
+        this.ethereum.on("accountsChanged", handleAccountsChanged)
+    }
+}
 
 
+let accounts;
+
+const handleAccountsChanged = (_accounts) => {
+    console.log("accounts changed")
+}
 
 async function checkBalance() {
     let balance = await window.ethereum.request({
-        method: "eth_getBalance ",
-
+        method: "eth_getBalance",
         params: [
+            window.userWalletAddress,
+            'latest'
 
         ]
+    }).catch((err) => {
+        console.log(err.code)
     })
+    console.log(window.userWalletAddress)
+    console.log(parseInt(balance) / Math.pow(10, 18))
 }
